1f0aefebab6f80212239649672c5526933da12a7
Iliyafingili <iliyafingili@gmail.com>
2025-08-10 21:43:29 +0330

    update_app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 import './bootstrap';
+
+function setupDropdown(buttonId, menuId, otherMenus = []) {
+    const button = document.getElementById(buttonId);
+    const menu = document.getElementById(menuId);
+
+    // کلیک روی دکمه
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // بستن سایر منوها
+        otherMenus.forEach(otherMenu => otherMenu.classList.remove('active'));
+
+        // باز و بسته کردن این منو
+        menu.classList.toggle('active');
+    });
+
+    // کلیک بیرون منو → بستن
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && event.target !== button) {
+            menu.classList.remove('active');
+        }
+    });
+}
+
+// اجرای تابع برای دو منو
+const dropdown_menu_categorie = document.getElementById('dropmenu-categories');
+const dropdown_menu_content_us = document.getElementById('dropmenu-content-us');
+
+setupDropdown('categories', 'dropmenu-categories', [dropdown_menu_content_us]);
+setupDropdown('content-us', 'dropmenu-content-us', [dropdown_menu_categorie]);
