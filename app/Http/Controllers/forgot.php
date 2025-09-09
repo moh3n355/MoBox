118ae3847db07547d3322d87631b016791def6f2
Moh3n <moh3n355@gmail.com>
2025-09-09 22:52:42 +0330

    finish general of step one

--- app/Http/Controllers/forgot.php
+++ app/Http/Controllers/forgot.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class forgot extends Controller
-{
-    
+{   
+    public function CheckUserNumber($request){
+        if(User::where('phone', $request->input('phone'))->exists()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function CreatePassword($request){
+
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $password = '';
+
+    for ($i = 0; $i <8; $i++) {
+        $NwePassword .= $characters[rand(0, strlen($characters) - 1)];
+    }
+
+    $user = User::where('phone', $request->input('phone'))->first();
+    $user->userpassword = bcrypt($NwePassword);
+    return redirect()->route('')->with('NwePassword', $NwePassword);
+    }
 }
