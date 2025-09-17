0982eff5b444ff95387cdc2e8327cac2418def6e
Moh3n <moh3n355@gmail.com>
2025-09-17 21:31:51 +0330

    add migreations and models

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function cartItems(){
+    return $this->hasMany(Cart::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
