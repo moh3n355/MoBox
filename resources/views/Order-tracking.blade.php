535447e87c9f91da5a8132dbe8a753b99aa2f450
Iliyafingili <iliyafingili@gmail.com>
2025-09-24 17:29:14 +0330

    update order track styles

--- resources/views/Order-tracking.blade.php
+++ resources/views/Order-tracking.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/order-tracking.css', 'resources/js/order-tracking.js'])
+    @vite(['resources/css/order-tracking.css', 'resources/js/order-tracksing.js'])
 
 </head>
 <body>
@@ -17,6 +17,7 @@
             <h1>پیگیری سفارش</h1>
 
             <div class="steps-labels">
+                <span>پرداخت</span>
                 <span>ثبت سفارش</span>
                 <span>آماده‌سازی</span>
                 <span>ارسال</span>
@@ -26,12 +27,13 @@
 
             <!-- progress bar -->
             <div class="progress-container">
-                <div class="progress" id="progress"></div>
-                <div class="circle done">1</div>
-                <div class="circle active">2</div>
-                <div class="circle">3</div>
-                <div class="circle">4</div>
-            </div>
+                <div class="progress" id="progress" style="width:50"></div>
+                <div class="circle done" id="step1">1</div>
+                <div class="circle done" id="step1">2</div>
+                <div class="circle active" id="step2">3</div>
+                <div class="circle" id="step3">4</div>
+                <div class="circle" id="step4">5</div>
+              </div>
 
 
 
